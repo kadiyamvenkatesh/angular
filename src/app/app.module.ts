08b6067d0a7626dde70cb27ce07af9596fc89870
kadiyamvenkatesh <kadiyamvenkatesh3986@gmail.com>
2023-05-08 23:44:59 +0530

    commiting angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import { SearchComponent } from './search/search.component'
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { PopupComponent } from './popup/popup.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatDialogModule} from '@angular/material/dialog';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SearchComponent,
+    PopupComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    Ng2SearchPipeModule,
+    BrowserAnimationsModule,
+    MatDialogModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+  
+ }
---
+++ src/app/ddl.sql/ddl.sql
@@ -0,0 +1,7 @@
+CREATE TABLE mobile_details
+(
+    mobile_number INT,
+ mobile_name Varchar (50) ,
+  manufaturing _year INT,
+   mobile_cost INT 
+);
